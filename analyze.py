d23ed192247e4885e4adddcaa5a3489d46ca1352
sgr-xd <nerdzspot@gmail.com>
2024-03-15 11:15:21 +0530

    signature_extraction_completed

--- analyze.py
+++ analyze.py
@@ -1,8 +1,9 @@
 from extract_text import detect_text
 from gemini_ai import gemini_classification
-
+from sig import signature_extraction_start
 def analyze(filepath):
     raw_text = detect_text(filepath)
     cheque_details = gemini_classification(raw_text)
     print(cheque_details)  #continue from here. cheque_details has the json of all extracted fields from the cheque
+    signature_extraction_start(filepath)
     return "done"
